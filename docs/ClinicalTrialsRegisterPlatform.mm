0f63defc51c1625f24680720e5746eb5af28d444
ramalho <luciano@ramalho.org>
2009-07-17 16:53:46 +0000

    uploaded first project mindmap

---
+++ docs/ClinicalTrialsRegisterPlatform.mm
@@ -0,0 +1,83 @@
+<map version="0.7.1">
+<node TEXT="Clinical Trials Register Platform">
+<node TEXT="integration" POSITION="right">
+<node TEXT="UTRN assignment"/>
+<node TEXT="export">
+<node TEXT="WHO Central Repo"/>
+</node>
+<node TEXT="import">
+<node TEXT="collaborating CTRs"/>
+</node>
+<node TEXT="duplicate detection">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node TEXT="collaborative data dictionary evolution"/>
+<node TEXT="search">
+<node TEXT="local database"/>
+<node TEXT="remote information sources">
+<icon BUILTIN="flag"/>
+</node>
+</node>
+</node>
+<node TEXT="customization" POSITION="left">
+<node TEXT="record schema"/>
+</node>
+<node TEXT="database" POSITION="right">
+<node TEXT="schema">
+<node TEXT="CT records">
+<node TEXT="ICTRP data set"/>
+<node TEXT="suggested fields"/>
+<node TEXT="custom fields"/>
+<node TEXT="translations"/>
+<node TEXT="changelog"/>
+<node TEXT="annotations">
+<node TEXT="record-level"/>
+<node TEXT="field-level"/>
+</node>
+</node>
+<node TEXT="users"/>
+<node TEXT="information sources">
+<icon BUILTIN="flag"/>
+</node>
+</node>
+<node TEXT="modelling">
+<node TEXT="multi-table inheritance"/>
+<node TEXT="generic key-value tables"/>
+<node TEXT="schema evolution"/>
+</node>
+</node>
+<node TEXT="publication" POSITION="left">
+<node TEXT="workflow">
+<node TEXT="states">
+<node TEXT="draft"/>
+<node TEXT="pending"/>
+<node TEXT="published"/>
+<node TEXT="rejected"/>
+</node>
+<node TEXT="roles"/>
+</node>
+<node TEXT="duplicate detection">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node TEXT="content types">
+<node TEXT="CT records"/>
+<node TEXT="CT record annotations"/>
+<node TEXT="other"/>
+</node>
+<node TEXT="translation"/>
+</node>
+<node TEXT="CMS framework" POSITION="left">
+<node TEXT="open source"/>
+<node TEXT="criteria (Vision p. 13-14)"/>
+<node TEXT="internationalization"/>
+</node>
+<node TEXT="instances" POSITION="left">
+<node TEXT="LA&amp;C RCTR"/>
+<node TEXT="REBRAC"/>
+</node>
+<node TEXT="open source m.o." POSITION="left">
+<node TEXT="LGPL"/>
+<node TEXT="open development process"/>
+</node>
+</node>
+</map>
