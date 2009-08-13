5052d315b7a81985d305a2de94ed1159a44f9e71
ramalho <luciano@ramalho.org>
2009-08-13 21:49:49 +0000

    template with user detection

--- clinicaltrials/rebrac/views.py
+++ clinicaltrials/rebrac/views.py
@@ -3,5 +3,9 @@
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response('rebrac/index.html')
+    username = request.user.username if request.user.is_authenticated() else None
+    return render_to_response('rebrac/index.html', locals())
 
+def user_dump(request):
+    uvars = [{'k':k, 'v':v} for k, v in request.user.__dict__.items()]
+    return render_to_response('rebrac/user_dump.html', locals())
