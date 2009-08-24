2b1550ccdd61f406fec128b62050c1793c06cdfa
fabiobatalha <fabio.batalha@gmail.com>
2009-08-24 14:29:39 +0000

    creating form to add data to registry database

--- registry/views.py
+++ registry/views.py
@@ -3,6 +3,7 @@ from django.template import Context, loader
 from django.shortcuts import render_to_response
 from clinicaltrials.registry.models import ClinicalTrial
 from django.http import HttpResponse
+from django import forms
 
 def index(request):
     latest_clinicalTrials = ClinicalTrial.objects.all().order_by('dateFirst_enrollment')[:5]
@@ -12,6 +13,21 @@ def index(request):
     })
     return HttpResponse(t.render(c))
 
+#def add(request):
+#    t = loader.get_template('registry/add_clinicalTrials.html')
+#    return render_to_response('registry/add_clinicalTrials.html')
+    
+class ClinicalTrialForm(forms.ModelForm):
+    class Meta:
+        model = ClinicalTrial
+
 def add(request):
-    #t = loader.get_template('registry/add_clinicalTrials.html')
-    return render_to_response('registry/add_clinicalTrials.html') 
+    if request.method == 'POST':
+        form = ClinicalTrialForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render_to_response('registry/add_done.html', form.cleaned_data)
+    else:
+        form = ClinicalTrialForm()
+    vars = {'form':form}
+    return render_to_response('registry/add_clinicalTrials.html', vars)
