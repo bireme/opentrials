e69b44253da842faca9aa9f1288570c16a43a92a
fabiobatalha <fabio.batalha@gmail.com>
2009-08-24 14:29:53 +0000

    creating form to add data to registry database

--- clinicaltrials/registry/urls.py
+++ clinicaltrials/registry/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls.defaults import *
+from clinicaltrials.registry.models import ClinicalTrial
+
+info_dict = {
+    'queryset': ClinicalTrial.objects.all(),
+}
+
 
 urlpatterns = patterns('',
-    (r'add/$', 'clinicaltrials.registry.views.add'),   
-    (r'$', 'clinicaltrials.registry.views.index'),    
+    (r'^add/$', 'clinicaltrials.registry.views.add'),   
+    (r'^$', 'clinicaltrials.registry.views.index'),
+    (r'^list/$', 'django.views.generic.list_detail.object_list', info_dict),
 )
