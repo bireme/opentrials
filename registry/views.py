2e48b1ea5160a0a84e0a174ead7fdea804d395a4
ramalho <luciano@ramalho.org>
2009-08-27 19:08:56 +0000

    reorganized registry/fixtures/initial_data.json to avoid integrity error (ClinicalTrial table must be loaded before the RecruitmentCountry table); changed dateFirst_enrollment to dt_first_enrollment

--- registry/views.py
+++ registry/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django import forms
 
 def index(request):
-    latest_clinicalTrials = ClinicalTrial.objects.all().order_by('dateFirst_enrollment')[:5]
+    latest_clinicalTrials = ClinicalTrial.objects.all().order_by('dt_first_enrollment')[:5]
     t = loader.get_template('registry/latest_clinicalTrials.html')
     c  = Context({
         'latest_clinicalTrials': latest_clinicalTrials,
