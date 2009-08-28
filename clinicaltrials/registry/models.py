f46a91f9f4ae44af5ce35dcf099bf37aa6ee7ad0
ramalho <luciano@ramalho.org>
2009-08-28 16:50:20 +0000

    start of localization process; manage.py makemessages requires xgettext which is not available on Windows

--- clinicaltrials/registry/models.py
+++ clinicaltrials/registry/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.forms import ModelForm
-
+from django.utils.translation import ugettext as _
 
 from vocabularies import COUNTRIES
 
@@ -13,15 +13,18 @@ class RecruitmentStatus(models.Model):
         return u'%s' % (self.label)
     
     class Meta:
-        verbose_name_plural = u'Recruitment status'
+        verbose_name_plural = _('Recruitment status')
     
 class RecruitmentCountry(models.Model):
     trial = models.ForeignKey('ClinicalTrial')
     country_code = models.CharField(max_length=8, choices=COUNTRIES)
     
 class ClinicalTrial(models.Model):
-    public_title         = models.CharField(max_length=255, db_index=True)
-    scientific_title     = models.CharField(max_length=255, db_index=True)
-    dt_first_enrollment = models.DateField()
-    target_sample_size   = models.IntegerField()
-    recruitment_status   = models.ForeignKey('RecruitmentStatus')
+    public_title         = models.CharField(_('Public Title'),
+                                            max_length=255, db_index=True)
+    scientific_title     = models.CharField(_('Scientific Title'),
+                                            max_length=255, db_index=True)
+    dt_first_enrollment  = models.DateField(_('Date of First Enrollment'))
+    target_sample_size   = models.IntegerField(_('Target Sample Size'))
+    recruitment_status   = models.ForeignKey('RecruitmentStatus',
+                                             verbose_name=_('Recruitment Status'))
