6428f586ee12d500ab9d55fbadb24a2a7125b86c
ramalho <luciano@ramalho.org>
2009-09-29 21:58:58 +0000

    included inline for secondary sponsor and source of monetary support

--- clinicaltrials/registry/admin.py
+++ clinicaltrials/registry/admin.py
@@ -10,15 +10,19 @@ class OutcomeInline(admin.StackedInline):
     
 class SecondaryNumberInline(admin.TabularInline):
     model = TrialNumber
+
+class TrialInstitutionInline(admin.TabularInline):
+    model = TrialInstitution
+    
     
 class ClinicalTrialAdmin(admin.ModelAdmin):
     inlines = [SecondaryNumberInline, RecruitmentCountryInline, 
-               OutcomeInline]
+               OutcomeInline, TrialInstitutionInline]
     list_display = ('updated_str','identifier','short_title','record_status',)
     list_display_links = ('identifier','short_title',)
     list_filter = ('record_status','study_type','phase',
                    'recruitment_status',)
     search_fields = ('scientific_title', 'public_title', 'i_freetext',)
-       
+              
 admin.site.register(ClinicalTrial, ClinicalTrialAdmin)
-admin.site.register(Institution)
+admin.site.register(Contact)
