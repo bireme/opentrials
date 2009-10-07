91036733e599f9cddc4198078d2d8137974724fa
fabiomontefuscolo <fabio.montefuscolo@gmail.com>
2009-10-07 21:42:42 +0000

    Contact form added

--- registry/trds_forms.py
+++ registry/trds_forms.py
@@ -3,8 +3,32 @@
 from django import forms
 from django.utils.translation import ugettext as _
 
+from models import Institution
+
 import choices
 from vocabulary.models import CountryCode, RecruitmentStatus, StudyType, StudyPhase
+
+
+class ContactForm(forms.Form):
+    title = _('Contacts')
+
+    firstname = forms.CharField(label=_('First Name'), max_length=50)
+    middlename = forms.CharField(label=_('Middle Name'), max_length=50)
+    lastname = forms.CharField(label=_('Last Name'), max_length=50)
+    
+    email = forms.EmailField(label=_('E-mail'), max_length=255)
+
+    affiliation = forms.ModelChoiceField(Institution.objects.all(),
+                                         _('Affiliation'))
+
+    address = forms.CharField(label=_('Address'), max_length=255)
+    city = forms.CharField(label=_('City'), max_length=255)
+
+    country = forms.ModelChoiceField(CountryCode.objects.all(), _('Country'))
+
+    zip = forms.CharField(label=_('Postal Code'), max_length=50)
+    telephone = forms.CharField(label=_('Telephone'), max_length=255)
+
 
 class TrialIdentificationForm(forms.Form):
     title = _('Trial Identifcation')
