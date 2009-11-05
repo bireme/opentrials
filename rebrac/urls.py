28992663a2b1ca72613d2ad575072a36331681e4
fabiobatalha <fabio.batalha@gmail.com>
2009-11-05 21:54:44 +0000

    Submission list and detail using generic views

--- rebrac/urls.py
+++ rebrac/urls.py
@@ -1,11 +1,24 @@
 from django.conf.urls.defaults import *
 from django.core.urlresolvers import reverse
 from django.contrib.auth.views import login, logout
+from django.views.generic import list_detail
 
 from rebrac.views import index, user_dump
+from rebrac.models import Submission
+
+
+submissions = {
+   'queryset':Submission.objects.all()
+}
 
 urlpatterns = patterns('',
-    url(r'^accounts/profile/$', user_dump, name='rebrac.userhome'),    
+    url(r'^accounts/profile/$', list_detail.object_list,
+        submissions, name='rebrac.userhome'),
+    url(r'^accounts/submissionlist/$', list_detail.object_list,
+        submissions, name='rebrac.submissionlist'), #same of accounts/profile
+    url(r'^accounts/submission/(?P<object_id>\d+)/$', list_detail.object_detail,
+        submissions, name='rebrac.submission'), #same of accounts/profile    
+    url(r'^accounts/userdump/$', user_dump),
     url(r'^accounts/login/$', login, 
         dict(template_name='rebrac/login.html'),
         name='rebrac.login'),
