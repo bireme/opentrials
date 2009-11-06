8b2e6bfa28251e9c6120264bcf0e04a365ba0e04
fabiobatalha <fabio.batalha@gmail.com>
2009-11-06 20:19:14 +0000

    Related to ticket #14 using generic views to create a XML export based on ICTRP DTD format.

--- registry/urls.py
+++ registry/urls.py
@@ -1,14 +1,22 @@
 from django.conf.urls.defaults import *
-from clinicaltrials.registry.models import ClinicalTrial
+from django.views.generic import list_detail
+
+from registry.models import ClinicalTrial
 
 info_dict = {
     'queryset': ClinicalTrial.objects.all(),
 }
-
+info_dict_xml = {
+    'queryset': ClinicalTrial.objects.all(),    
+    'template_name': 'registry/clinicaltrial_detail.xml',
+    'mimetype': 'text/xml',
+}
 
 urlpatterns = patterns('',
-    (r'^edit/(\d+)/$', 'clinicaltrials.registry.views.edit_trial_index'),   
-    (r'^form/(\d+)/(\w+)/$', 'clinicaltrials.registry.views.edit_trial_form'),   
-    (r'^$', 'clinicaltrials.registry.views.index'),
-    (r'^list/$', 'django.views.generic.list_detail.object_list', info_dict),
+    url(r'^edit/(\d+)/$', 'clinicaltrials.registry.views.edit_trial_index'),
+    url(r'^xml/(?P<object_id>\d+)/$', list_detail.object_detail, info_dict_xml,
+        name='registry.xml'),
+    url(r'^form/(\d+)/(\w+)/$', 'clinicaltrials.registry.views.edit_trial_form'),
+    url(r'^$', 'clinicaltrials.registry.views.index'),
+    url(r'^list/$', 'list_detail.object_list', info_dict),
 )
