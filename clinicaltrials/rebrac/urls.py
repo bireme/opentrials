a936cee60f9d01b82c8751830353b6ed6ef3e04a
ramalho <luciano@ramalho.org>
2009-11-08 20:55:51 +0000

    changed urls.py files in registry and rebrac to use view function references (not strings); created test_users.json and admin_user.json fixtures in rebrac app

--- clinicaltrials/rebrac/urls.py
+++ clinicaltrials/rebrac/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from django.core.urlresolvers import reverse
 from django.contrib.auth.views import login, logout
-from django.views.generic import list_detail
+from django.views.generic.list_detail import object_list, object_detail
 
 from rebrac.views import index, user_dump
 from rebrac.models import Submission
@@ -12,18 +12,16 @@ submissions = {
 }
 
 urlpatterns = patterns('',
-    url(r'^accounts/profile/$', list_detail.object_list,
-        submissions, name='rebrac.userhome'),
-    url(r'^accounts/submissionlist/$', list_detail.object_list,
-        submissions, name='rebrac.submissionlist'), #same of accounts/profile
-    url(r'^accounts/submission/(?P<object_id>\d+)/$', list_detail.object_detail,
-        submissions, name='rebrac.submission'),
+    url(r'^accounts/profile/$', object_list, submissions, 
+        name='rebrac.userhome'),
+    url(r'^accounts/submissionlist/$', object_list, submissions, 
+        name='rebrac.submissionlist'), #same of accounts/profile
+    url(r'^accounts/submission/(?P<object_id>\d+)/$', object_detail, submissions, 
+        name='rebrac.submission'),
     url(r'^accounts/userdump/$', user_dump),
-    url(r'^accounts/login/$', login, 
-        dict(template_name='rebrac/login.html'),
+    url(r'^accounts/login/$', login, dict(template_name='rebrac/login.html'),
         name='rebrac.login'),
-    url(r'^accounts/logout/$', logout, 
-        dict(next_page='/'),
+    url(r'^accounts/logout/$', logout, dict(next_page='/'),
         name='rebrac.logout'),
     url(r'^$', index, name='rebrac.home'),  
 )
--- clinicaltrials/registry/urls.py
+++ clinicaltrials/registry/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
-from django.views.generic import list_detail
+from django.views.generic.list_detail import object_detail, object_list
 
 from registry.models import ClinicalTrial
+from registry.views import edit_trial_index, edit_trial_form, index
 
 info_dict = {
     'queryset': ClinicalTrial.objects.all(),
@@ -13,10 +14,10 @@ info_dict_xml = {
 }
 
 urlpatterns = patterns('',
-    url(r'^edit/(\d+)/$', 'clinicaltrials.registry.views.edit_trial_index'),
-    url(r'^xml/(?P<object_id>\d+)/$', list_detail.object_detail, info_dict_xml,
+    url(r'^edit/(\d+)/$', edit_trial_index),
+    url(r'^xml/(?P<object_id>\d+)/$', object_detail, info_dict_xml,
         name='registry.xml'),
-    url(r'^form/(\d+)/(\w+)/$', 'clinicaltrials.registry.views.edit_trial_form'),
-    url(r'^$', 'clinicaltrials.registry.views.index'),
-    url(r'^list/$', 'list_detail.object_list', info_dict),
+    url(r'^form/(\d+)/(\w+)/$', edit_trial_form),
+    url(r'^$', index),
+    url(r'^list/$', object_list, info_dict),
 )
