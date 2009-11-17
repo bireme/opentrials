191a7053e3d9e3f7a36202710ac17964b57d1e9c
fabiomontefuscolo <fabio.montefuscolo@gmail.com>
2009-11-17 19:34:47 +0000

    Submission creation implemented

--- rebrac/urls.py
+++ rebrac/urls.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.views import login, logout
 from django.views.generic.list_detail import object_list, object_detail
 
-from rebrac.views import index, user_dump
+from rebrac.views import index, user_dump, new_submission
 from rebrac.models import Submission
 
 
@@ -12,16 +12,25 @@ submissions = {
 }
 
 urlpatterns = patterns('',
-    url(r'^accounts/profile/$', object_list, submissions, 
+    url(r'^accounts/profile/$', object_list, submissions,
         name='rebrac.userhome'),
+
     url(r'^accounts/submissionlist/$', object_list, submissions, 
         name='rebrac.submissionlist'), #same of accounts/profile
+
     url(r'^accounts/submission/(?P<object_id>\d+)/$', object_detail, submissions, 
         name='rebrac.submission'),
+
+    url(r'^accounts/newsubmission/$', new_submission,
+        name='rebrac.new_submission'),
+
     url(r'^accounts/userdump/$', user_dump),
+
     url(r'^accounts/login/$', login, dict(template_name='rebrac/login.html'),
         name='rebrac.login'),
+
     url(r'^accounts/logout/$', logout, dict(next_page='/'),
         name='rebrac.logout'),
+        
     url(r'^$', index, name='rebrac.home'),  
 )
--- rebrac/views.py
+++ rebrac/views.py
@@ -1,6 +1,12 @@
 # coding: utf-8
-
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+
+
+from rebrac.forms import SubmissionForm
+from rebrac.models import Submission
+from registry.models import ClinicalTrial
 
 def index(request):
     username = request.user.username if request.user.is_authenticated() else None
@@ -9,3 +15,32 @@ def index(request):
 def user_dump(request):
     uvars = [{'k':k, 'v':v} for k, v in request.user.__dict__.items()]
     return render_to_response('rebrac/user_dump.html', locals())
+
+def new_submission(request):
+#    import pdb
+#    pdb.set_trace()
+#    
+    if request.method == 'POST': # If the form has been submitted...
+        form = SubmissionForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            pass
+            ct = ClinicalTrial()
+
+            for name,value in request.POST.items():
+                if hasattr(ct, name):
+                    setattr(ct, name, value)
+            ct.save()
+
+            sub = Submission()
+            sub.creator = request.user
+            sub.trial = ct
+
+            sub.save()           
+
+            return HttpResponseRedirect(reverse('rebrac.userhome')) # Redirect after POST
+    else:
+        form = SubmissionForm() # An unbound form
+
+    return render_to_response('rebrac/new_submission.html', {
+        'form': form,
+    })
