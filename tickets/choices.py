30c4262287a3fb2419ffb561c3152d9ec2ab58e0
fabiobatalha <fabio.batalha@gmail.com>
2009-12-09 20:24:34 +0000

    Adding the followup model to tickets app

--- tickets/choices.py
+++ tickets/choices.py
@@ -2,7 +2,9 @@
 
 ## Qualifiers for the relationship of secondary entities with a Tickets #
 
-TICKET_SUBJECT = [
-    ('review', 'Ask for review'), #TRDS 4
-    ('followup', 'Followup'), #TRDS 6
+TICKET_CONTEXT = [
+    ('ct_registry', 'Clinical Trial Registry',),
+    ('translation_tools', 'Administrative translation Tools',),
+    ('users_tools', 'Administrative translation Tools',),
+    ('other', 'Other Context',),
 ]
