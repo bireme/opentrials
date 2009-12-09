e1e2469a384370e4311ac73d43f3d502fac2a9c2
fabiobatalha <fabio.batalha@gmail.com>
2009-12-09 17:19:07 +0000

    Tickets APP working in Admin interface

---
+++ tickets/choices.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+## Qualifiers for the relationship of secondary entities with a Tickets #
+
+TICKET_SUBJECT = [
+    ('review', 'Ask for review'), #TRDS 4
+    ('followup', 'Followup'), #TRDS 6
+]
--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+#import datetime
+
+#from utilities import safe_truncate
+
+#from vocabulary.models import CountryCode, StudyPhase, StudyType, RecruitmentStatus, InterventionCode
+
+#import choices
+
+class Ticket(models.Model):
+    date_registration = models.DateField(_('Date of Registration'), null=True,
+                                         editable=False, db_index=True)
+    date_iteration = models.DateField(_('Date of Iteration'), null=True,
+                                         db_index=True)
+    subject = models.TextField(_('Subject'), max_length=256, db_index=True)
+    description = models.TextField(_('Description'), max_length=2000)
+    owner = models.TextField(_('Request Owner'), max_length=256, db_index=True)
+    from_user = models.TextField(_('From user'), max_length=256, db_index=True)
+    to_user = models.TextField(_('To User'), max_length=256, db_index=True)
+    status = models.TextField(_('Ticket Status'), max_length=256, db_index=True)
+    #parent_relation = models.
+
+#    class Meta:
+ #       order_with_respect_to = 'trial'
+
+    def __unicode__(self):
+        return u'%s' % (self.subject)
