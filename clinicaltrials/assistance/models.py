a620bba0bfb7c131d2ee23dc05b484000f68b6a7
jamilatta <atta.jamil@gmail.com>
2009-12-14 18:32:16 +0000

    added template view

--- clinicaltrials/assistance/models.py
+++ clinicaltrials/assistance/models.py
@@ -3,21 +3,29 @@ from django.utils.translation import ugettext as _
 from datetime import datetime
 
 class Category(models.Model):
-    
-    category = models.CharField(_('Category'), max_length=256)
+    class Meta:
+        verbose_name_plural = _('Categories')
+
+    label = models.CharField(_('Label'), max_length=255, unique=True)
     
     def __unicode__(self):
-        return self.category
+        return self.label
 
 
 class Question(models.Model):
 
     category = models.ForeignKey(Category)
-    question = models.TextField(_('Question'), max_length=2000)
+    title = models.TextField(_('Title'), max_length=2000)
     answer = models.TextField(_('Answer'), max_length=2000)
-    created = models.DateTimeField(_('Date of Registration'),default=datetime.now,
+    order = models.PositiveIntegerField(default=0)
+    created = models.DateTimeField(_('Date of Registration'), default=datetime.now,
         editable=False)
 
+    def save(self):
+        super(Question, self).save()
+        if self.order == 0:
+            self.order = self.id*10
+            self.save()
+
     def __unicode__(self):
-        return self.question
-    
+        return self.title
