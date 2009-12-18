e641c1dce926e703ca71eea75bed9e4c566212a1
fabiobatalha <fabio.batalha@gmail.com>
2009-12-18 19:04:16 +0000

    Developed features related to open tickets

--- clinicaltrials/tickets/urls.py
+++ clinicaltrials/tickets/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_detail, object_list
 from tickets.models import Ticket
-from tickets.views import index, new_iteration, reopen_ticket, resolve_ticket, close_ticket
+from tickets.views import index, new_iteration, reopen_ticket, resolve_ticket, close_ticket, open_ticket
 
 
 info_dict = {
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^$', index),
     url(r'^list/$', object_list, info_dict),
     url(r'^history/(?P<object_id>\d+)/$', object_detail, info_dict, name='ticket.history' ),
-    #url(r'^open/$', open_ticket, name='ticket.open' ),
+    url(r'^open/(?P<context>\w+)/(?P<type>\w+)/$', open_ticket, name='ticket.open' ),
     url(r'^reopen/(?P<object_id>\d+)/$', reopen_ticket, name='ticket.reopen' ),
     url(r'^resolve/(?P<object_id>\d+)/$', resolve_ticket, name='ticket.resolve' ),
     url(r'^close/(?P<object_id>\d+)/$', close_ticket, name='ticket.close' ),
