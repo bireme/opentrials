d2d13e3748349e8c7262248c89688ccfc9ea2ae4
ramalho <luciano@ramalho.org>
2010-01-14 18:11:28 +0000

    continued implementation of Items and Attributes

--- infobase/models.py
+++ infobase/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 
 from datetime import datetime
+from hashlib import md5
 
 class Item(models.Model):
     key = models.CharField(max_length=255, unique=True)
-    type = models.ForeignKey(self, index=True)
     revision = models.PositiveIntegerField(default=0)
     created = models.DateTimeField(default=datetime.now, index=True)
     updated = models.DateTimeField(null=True, blank=True, index=True)
     
+    def __init__(self, **kwargs):
+        super(Item, self).__init__(**kwargs)
+        m = hashlib.md5()
+        m.update(repr(kwargs))
+        super(Item, self).__setattr__('key', m.hexdigest())
+    
     def save(self):
         if self.id:
             self.updated = datetime.now()
-
+            
     def __getattr__(self, attr_name):
         ''' get named attribute of current version '''
 
@@ -23,7 +29,7 @@ class Item(models.Model):
         next_revision = self.revision + 1
         other_attrs = Attribute.objects.filter(item=self,revision=self.revision).exclude(key=attr_name)
         other_attrs.update(revision=next_revision)
-        self.revision = next_revision
+        super(Item, self).__setattr__('revision', next_revision)
         self.save()
         Attribute.objects.create(item=self, revision=next_revision, key=attr_name)
 
