05f95a9692ab9d1249525ae678c8e80a00de04e8
fabiomontefuscolo <fabio.montefuscolo@gmail.com>
2010-03-10 20:41:28 +0000

    put django i18n and url reverse on javascript code, decouple js code from template

--- decsclient/urls.py
+++ decsclient/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
-    url(r'^getterm/(?P<lang>[a-z]{2,2})/(?P<code>[A-Z](\d{2,2}(\.\d{3,3})*)?)?$', getterm),
+    url(r'^getterm/(?P<lang>[a-z]{2,2})/(?P<code>[A-Z](\d{2,2}(\.\d{3,3})*)?)?$', getterm, name='decs.getterm'),
     url(r'^search/(?P<lang>[a-z]{2,2})/(?P<prefix>[14]0[1-7])/(?P<term>.*)$', search),
-    url(r'^search/(?P<lang>[a-z]{2,2})/(?P<term>.*)$', search),
+    url(r'^search/(?P<lang>[a-z]{2,2})/(?P<term>.*)$', search, name='decs.search'),
     url(r'^test_search/$', test_search),
 )
