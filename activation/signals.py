666e39ab8eb41f7ea0d189e39ebcdaad387bef48
ramalho <luciano@ramalho.org>
2010-03-22 19:46:02 +0000

    added django-registration user account activation functionality

---
+++ activation/signals.py
@@ -0,0 +1,9 @@
+# this file was copied from django-registration/registration/signals.py
+
+from django.dispatch import Signal
+
+# A new user has registered.
+user_registered = Signal(providing_args=["user", "request"])
+
+# A user has activated his or her account.
+user_activated = Signal(providing_args=["user", "request"])
