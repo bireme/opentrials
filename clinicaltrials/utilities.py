5a6d11d6aba5dbff2920b8c70a6a0d77bb74549d
ramalho <luciano@ramalho.org>
2010-04-01 21:02:44 +0000

    revised httpd.conf files and utilities for checking deployment status

--- clinicaltrials/utilities.py
+++ clinicaltrials/utilities.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from django.http import HttpResponse
+from django.contrib.auth.decorators import user_passes_test
 
 def smoke_test(request):
     from datetime import datetime
     return HttpResponse(datetime.now().strftime('%H:%M:%S'))
 
+@user_passes_test(lambda u: u.is_staff)
 def req_dump(request):
     template = '''
     <form action="./" method="POST">
@@ -24,6 +26,23 @@ def req_dump(request):
         rows.append('<tr><th>%s</th><td>%s</td></tr>' % (k, request.POST[k]))
     return HttpResponse(template % ('\n'.join(rows)))
 
+@user_passes_test(lambda u: u.is_staff)
+def sys_info(request):
+    template = '''
+    <h1>settings path</h1>
+    <pre>%(settingspath)s</pre>
+    <h1>svn info</h1>
+    <pre>%(svninfo)s</pre>
+    <h1>sys.path</h1>
+    <pre>%(syspath)s</pre>
+    '''
+    import sys
+    import settings
+    from subprocess import Popen, PIPE
+    svnout, svnerr = Popen(['svn', 'info', settings.PROJECT_PATH], stdout=PIPE).communicate()
+    return HttpResponse(template % {'settingspath': settings.PROJECT_PATH,
+                                    'syspath':'\n'.join(sys.path), 
+                                    'svninfo':svnout or svnerr})    
 
 ELLIPSIS = u'\u2026'
 
