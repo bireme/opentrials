cf142dfc9f408a210983940dc55cd58706eb911f
ramalho <luciano@ramalho.org>
2010-04-10 22:35:29 +0000

    created package and added e-mail check for diagnostic; removed local copy of jquery, replacing with URL for serving from Google, along with JQuery UI

---
+++ clinicaltrials/diagnostic/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+
+from diagnostic.views import *
+
+urlpatterns = patterns('',
+    # Diagnostic views
+    url(r'^smoke/$', smoke_test),
+    url(r'^reqdump/$', req_dump),
+    url(r'^sysinfo/$', sys_info),
+    url(r'^error/$', raise_error),
+
+)
--- clinicaltrials/utilities.py
+++ clinicaltrials/utilities.py
@@ -1,49 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from django.http import HttpResponse
-from django.contrib.auth.decorators import user_passes_test
-
-def smoke_test(request):
-    from datetime import datetime
-    return HttpResponse(datetime.now().strftime('%H:%M:%S'))
-
-@user_passes_test(lambda u: u.is_staff)
-def req_dump(request):
-    template = '''
-    <form action="./" method="POST">
-    <input type="text" name="word" value="mitochondrial">
-    <input type="submit" name="btn1" value="one">
-    <input type="submit" name="btn2" value="two">
-    </form>
-    <table border="1">
-       <tr><th>key</th><th>POST[key]</th></tr>
-    %s
-    </table>
-    '''
-    rows = []
-    for k in request.POST.keys():
-        rows.append('<tr><th>%s</th><td>%s</td></tr>' % (k, request.POST[k]))
-    return HttpResponse(template % ('\n'.join(rows)))
-
-@user_passes_test(lambda u: u.is_staff)
-def sys_info(request):
-    template = '''
-    <h1>settings path</h1>
-    <pre>%(settingspath)s</pre>
-    <h1>svn info</h1>
-    <pre>%(svninfo)s</pre>
-    <h1>sys.path</h1>
-    <pre>%(syspath)s</pre>
-    '''
-    import sys
-    import settings
-    from subprocess import Popen, PIPE
-    svnout, svnerr = Popen(['svn', 'info', settings.PROJECT_PATH], stdout=PIPE).communicate()
-    return HttpResponse(template % {'settingspath': settings.PROJECT_PATH,
-                                    'syspath':'\n'.join(sys.path), 
-                                    'svninfo':svnout or svnerr})    
-
 ELLIPSIS = u'\u2026'
 
 def safe_truncate(text, max_length=60, ellipsis=ELLIPSIS, encoding='utf-8',
