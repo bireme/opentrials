d8b39d4d342834f35a5551c8ef2ae204494354ef
ramalho <luciano@ramalho.org>
2010-04-20 20:53:02 +0000

    added backup script; removed empty models.py from decsclient app

---
+++ clinicaltrials/tools/dumpall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+../manage.py dumpdata --indent=2 assistance > bkp/assistance.json
+../manage.py dumpdata --indent=2 registration > bkp/registration.json
+../manage.py dumpdata --indent=2 repository > bkp/repository.json
+../manage.py dumpdata --indent=2 reviewapp > bkp/reviewapp.json
+../manage.py dumpdata --indent=2 tickets > bkp/tickets.json
+../manage.py dumpdata --indent=2 vocabulary > bkp/vocabulary.json
+../manage.py dumpdata --indent=2 -e assistance -e registration -e repository -e reviewapp -e tickets -e vocabulary > bkp/other.json
+cd ../static/
+tar czvf attachments.tgz attachments/
+cd ../tools/
+mv ../static/attachments.tgz bkp/ 
+tar czvf bkp.tgz bkp/
+
+
+
