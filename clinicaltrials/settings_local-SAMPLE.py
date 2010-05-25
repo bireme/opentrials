ed080481bef88d0a35b9cc9a6a62529f1d7f3ed0
fabiomontefuscolo <fabio.montefuscolo@gmail.com>
2010-05-25 13:45:13 +0000

    change settings.py parameters, refers #91

--- clinicaltrials/settings_local-SAMPLE.py
+++ clinicaltrials/settings_local-SAMPLE.py
@@ -4,12 +4,14 @@ ADMINS = (
     ('Webmaster Ensaios Clinicos', 'appec@bireme.org'),
 )
 
-DATABASE_ENGINE = 'mysql'      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'clinicaltrials' # Or path to database file if using sqlite3.
-DATABASE_USER = 'tester'       # Not used with sqlite3.
-DATABASE_PASSWORD = 'puffpuff' # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default' : {
+        'ENGINE':'mysql',
+        'NAME':'clinicaltrials',
+        'USER':'tester',
+        'PASSWORD':'puffpuff',
+    }
+}
 
 SECRET_KEY = 'rmbg(!8sa@&8o9pnnd@*szm+axos_6r$)r48jc2r$^_8+wz)po'
 
