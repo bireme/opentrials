3c4c67256935a4d48a298ec5e169ed15f87c8538
fabiomontefuscolo <fabio.montefuscolo@gmail.com>
2010-05-28 14:49:34 +0000

    Remove some configurations from httpd.conf and copy django "media" folder inside clinicaltrials "static" folder, closes #102

--- settings.py
+++ settings.py
@@ -41,7 +41,7 @@ MEDIA_URL = '/static/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/static/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '*06=j&&^n71^a&%%3rs%7lla+^(n^v1w@@dp_rxvi#&(xo7meq'
