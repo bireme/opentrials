fd937acbd0584645924c2027d22b5ef0f24683ce
fabiomontefuscolo <fabio.montefuscolo@gmail.com>
2010-06-18 19:50:30 +0000

    implements Remark box for submission forms

--- settings.py
+++ settings.py
@@ -99,6 +99,7 @@ TEMPLATE_CONTEXT_PROCESSORS =(
     'django.core.context_processors.auth',
     'django.core.context_processors.i18n',
     'django.core.context_processors.csrf',
+    'django.core.context_processors.media',
     'context_processors.clinicaltrials.polyglot',
 )
 
