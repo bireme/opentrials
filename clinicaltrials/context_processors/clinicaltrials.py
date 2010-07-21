3d9cb517e3f411a8220d25ed14f0412b579b38ab
rafaelbuy <rafaelbuy@gmail.com>
2010-07-21 16:56:09 +0000

    Added option to use jquery locally, refs #156

--- clinicaltrials/context_processors/clinicaltrials.py
+++ clinicaltrials/context_processors/clinicaltrials.py
@@ -8,3 +8,11 @@ def polyglot(request):
     context_extra['MANAGED_LANGUAGES_LC'] = [c.lower() for c in settings.MANAGED_LANGUAGES]
 
     return context_extra
+    
+def jquery(request):
+    context_extra = {}
+
+    context_extra['JQUERY_URL'] = settings.JQUERY_URL
+    context_extra['JQUERY_UI_URL'] = settings.JQUERY_UI_URL
+
+    return context_extra
