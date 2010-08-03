82af6ac32341ef6d28ee59b3ed57a16c72573ef3
fabiomontefuscolo <fabio.montefuscolo@gmail.com>
2010-08-03 20:06:15 +0000

    Merge remote branch 'github/master'

--- opentrials/polyglot/multilingual_forms.py
+++ opentrials/polyglot/multilingual_forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.validators import EMPTY_VALUES
 
 from models import Translation
 import re
@@ -90,6 +92,54 @@ class MultilingualTextarea(BaseMultilingualWidget):
     def __init__(self, *args, **kwargs):
         super(MultilingualTextarea, self).__init__(widget_class=forms.Textarea, *args, **kwargs)
 
+class BaseMultilingualSelect(object):
+    display_language = 'en' # FIXME: shouldn't be settings.LANGUAGE_CODE?
+    instance = None
+    model = None
+    label_field = None
+
+    def get_translated_choices(self):
+        choices = []
+
+        # Loops in the model choice objects
+        for item in self.model.objects.all():
+            # If there is a translation...
+            try:
+                item_trans = item.translations.get(language__iexact=self.display_language)
+            # If there is no translation (so the original object is returned)
+            except ObjectDoesNotExist:
+                item_trans = item
+
+            choices.append((item.pk, getattr(item_trans, self.label_field)))
+
+        return choices
+
+class MultilingualSelect(forms.Select, BaseMultilingualSelect):
+    def __init__(self, display_language=None, model=None, label_field=None, attrs=None):
+        self.display_language = display_language or self.display_language
+        self.model = model
+        self.label_field = label_field
+
+        super(MultilingualSelect, self).__init__(attrs)
+
+    def render(self, name, value, attrs=None):
+        self.choices = self.get_translated_choices()
+
+        return super(MultilingualSelect, self).render(name, value, attrs)
+
+class MultilingualSelectMultiple(forms.SelectMultiple, BaseMultilingualSelect):
+    def __init__(self, display_language=None, model=None, label_field=None, attrs=None):
+        self.display_language = display_language or self.display_language
+        self.model = model
+        self.label_field = label_field
+
+        super(MultilingualSelectMultiple, self).__init__(attrs)
+
+    def render(self, name, value, attrs=None, choices=()):
+        self.choices = self.get_translated_choices()
+
+        return super(MultilingualSelectMultiple, self).render(name, value, attrs, choices)
+
 # FIELDS
 
 class MultilingualField(forms.Field):
@@ -109,4 +159,33 @@ class MultilingualCharField(MultilingualField):
 
 class MultilingualTextField(MultilingualField):
     widget = MultilingualTextarea
+
+class MultilingualModelChoiceField(MultilingualField):
+    widget = MultilingualSelect
+    model = None
+    label_field = None
+
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model')
+        self.label_field = kwargs.pop('label_field')
+
+        super(MultilingualModelChoiceField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if value in EMPTY_VALUES:
+            return None
+        try:
+            value = self.model.objects.get(pk=value)
+        except ObjectDoesNotExist:
+            raise ValidationError(self.error_messages['invalid_choice'])
+        return value
+
+class MultilingualModelMultipleChoiceField(MultilingualModelChoiceField):
+    widget = MultilingualSelectMultiple
+
+    def to_python(self, value):
+        if value in EMPTY_VALUES:
+            return None
+
+        return self.model.objects.filter(pk__in=value)
 
