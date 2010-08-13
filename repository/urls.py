e0c4aa76b55434d360cebb7b12144c4145d9ad32
rafaelbuy <rafaelbuy@gmail.com>
2010-08-13 21:45:08 +0000

    changed urls, added trial preview and improved search, close #167, close #168

--- repository/urls.py
+++ repository/urls.py
@@ -3,9 +3,9 @@ from django.views.generic.list_detail import object_detail, object_list
 
 from repository.models import ClinicalTrial
 
-from repository.views import edit_trial_index, full_view, index,step_1, step_2, step_3
+from repository.views import edit_trial_index, full_view, index, step_1, step_2, step_3
 from repository.views import step_4, step_5, step_6, step_7, step_8, step_9, new_institution
-from repository.views import list_all, details
+from repository.views import list_all, trial_registered, trial_view
 
 
 info_dict_xml = {
@@ -15,8 +15,9 @@ info_dict_xml = {
 }
 
 urlpatterns = patterns('',
+    url(r'^list/$', list_all, name='repository.list_all'),
     url(r'^edit/(\d+)/$', edit_trial_index, name='repository.edittrial'),
-    url(r'^view/(?P<trial_pk>\d+)/$', details, name='repository.trialview'),
+    url(r'^view/(?P<trial_pk>\d+)/$', trial_view, name='repository.trialview'),
     url(r'^xml/(?P<object_id>\d+)/$', object_detail, info_dict_xml,
         name='repository.xml'),
     url(r'^new_institution/$', new_institution, name='new_institution'),
@@ -29,7 +30,7 @@ urlpatterns = patterns('',
     url(r'^step_7/(\d+)/$', step_7, name='step_7'),
     url(r'^step_8/(\d+)/$', step_8, name='step_8'),
     url(r'^step_9/(\d+)/$', step_9, name='step_9'),
-    url(r'^$', index),
-    url(r'^list/$', list_all, name='repository.list_all'),
-    
+    #public
+    url(r'^(?P<trial_id>[0-9A-Za-z-]+)/$', trial_registered, name='repository.trial_registered'),
+    url(r'^$', index, name='repository.index'),
 )
