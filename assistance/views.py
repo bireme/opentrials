a40a684fe498d35cb57e0fd4069a5660ae86bead
marinho <marinho@gmail.com>
2010-09-22 13:39:26 +0000

    Improving assistence.views.faq to use better the database and cache resources

--- assistance/views.py
+++ assistance/views.py
@@ -8,7 +8,17 @@ from assistance.models import Category, CategoryTranslation
 
 
 def faq(request):
-    questions = Question.objects.all().order_by('category', 'order')
+    """
+    ATTENTION: This view has a complexity of O(question * N * 2) on database load, because each
+    question does 1 or 2 calls to database to get question and category translations.
+
+    It is prepared to get questions including category fields at once SQL call and using cache
+    to get translations as far as possible.
+
+    If the questions quantity grow a lot, this logic should be replaced to something that gets
+    all translations from database at once.
+    """
+    questions = Question.objects.select_related('category').order_by('category', 'order')
 
     if len(questions) < 1:
         object_list = None
@@ -18,7 +28,8 @@ def faq(request):
             q = Question()
             c = Category()
             try:
-                trans = question.translations.get(language__iexact=request.LANGUAGE_CODE)
+                #trans = question.translations.get(language__iexact=request.LANGUAGE_CODE)
+                trans = QuestionTranslation.objects.get_translation_for_object(request.LANGUAGE_CODE, question)
                 q.title = trans.title
                 q.answer = trans.answer
                 q.order = question.order
@@ -26,7 +37,8 @@ def faq(request):
             except QuestionTranslation.DoesNotExist:
                 q = question
             try:
-                trans = question.category.translations.get(language__iexact=request.LANGUAGE_CODE)
+                #trans = question.category.translations.get(language__iexact=request.LANGUAGE_CODE)
+                trans = CategoryTranslation.objects.get_translation_for_object(request.LANGUAGE_CODE, question.category)
                 c.label = trans.label
             except CategoryTranslation.DoesNotExist:
                 c = question.category
