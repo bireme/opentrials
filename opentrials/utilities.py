7432327d5ab6d91c46a89dbbea3db3f63a423d97
ramalho <luciano@ramalho.org>
2010-09-27 21:46:41 +0000

    added LGPL headers and files

--- opentrials/utilities.py
+++ opentrials/utilities.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+
+# OpenTrials: a clinical trials registration system
+#
+# Copyright (C) 2010 BIREME/PAHO/WHO, ICICT/Fiocruz e
+#                    Ministério da Saúde do Brasil
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 2.1 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from django.http import HttpResponse
 from django.core import serializers
