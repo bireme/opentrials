129613806187a0ae823f76a1c8fec7466aa6d2f4
rafaelbuy <rafaelbuy@gmail.com>
2010-10-08 19:10:53 +0000

    implemented ticket #33

--- opentrials/reviewapp/admin.py
+++ opentrials/reviewapp/admin.py
@@ -1,9 +1,39 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from settings import PROJECT_PATH
 from utilities import safe_truncate
-from reviewapp.models import Submission, RecruitmentCountry, Remark, News
+from reviewapp.models import Submission, RecruitmentCountry, Remark, News, Attachment
+
+class AdminFileLinkWidget(admin.widgets.AdminFileWidget):
+    """
+    A FileLinkField Widget that shows only the link to the file.
+    """
+    def __init__(self, attrs={}):
+        super(AdminFileLinkWidget, self).__init__(attrs)
+
+    def render(self, name, value, attrs=None):
+        output = []
+        if value and hasattr(value, "url"):
+            output.append('<a target="_blank" href="%s">%s</a>' % \
+                           (value.url.replace(PROJECT_PATH, u''), \
+                            value.url.replace(PROJECT_PATH, u'').split("/")[-1], \
+                           ))
+        return mark_safe(u''.join(output))
 
 class RecruitmentCountryInline(admin.TabularInline):
     model = RecruitmentCountry
+    
+class AttachmentInline(admin.TabularInline):
+    model = Attachment
+    fields = ('file',)
+    can_delete = False
+    max_num = 0
+    
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'file':
+            kwargs['widget'] = AdminFileLinkWidget
+        return super(AttachmentInline, self).formfield_for_dbfield(db_field,**kwargs)
 
 class SubmissionAdmin(admin.ModelAdmin):
     list_display = ('creator_username', 'short_title', 'status')
@@ -12,7 +42,7 @@ class SubmissionAdmin(admin.ModelAdmin):
     date_hierarchy = 'updated'
     save_on_top = True
     change_form_template = 'admin/submission_change_form.html'
-    inlines = [RecruitmentCountryInline]
+    inlines = [RecruitmentCountryInline, AttachmentInline]
     def save_model(self, request, instance, form, change):
         if change:
             instance.updater = request.user
