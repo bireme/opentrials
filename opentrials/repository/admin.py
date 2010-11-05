78a3abf9205591602fa6ffc596f6ac0678c515f0
rafaelbuy <rafaelbuy@gmail.com>
2010-11-05 19:55:50 +0000

    Implemented action in the admin to publish an clinical trial, close #206

--- opentrials/repository/admin.py
+++ opentrials/repository/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.utils.translation import ugettext as _
+
 from repository.models import *
 
 from polyglot.admin import TranslationInline, TranslationAdmin
@@ -24,9 +26,29 @@ class ClinicalTrialAdmin(admin.ModelAdmin):
     list_display = ('updated_str','identifier','short_title','recruitment_status',)
     list_display_links = ('identifier','short_title',)
     search_fields = ('scientific_title', 'public_title', 'i_freetext',)
-    list_filter = ('updated', 'study_type', 'phase', 'recruitment_status',)
+    list_filter = ('updated', 'study_type', 'phase', 'recruitment_status', 'status',)
     date_hierarchy = 'updated'
     save_on_top = True
+    actions = ['publish_action']
+    
+    def publish_action(self, request, queryset):
+        rows_updated = 0
+        
+        for obj in queryset:
+            if obj.status != 'published':
+                obj.status = 'published'
+                obj.save()
+                rows_updated += 1
+                
+        if rows_updated == 0:
+            message = _("No clinical trial was published.") 
+        elif rows_updated == 1:
+            message = "1 %s" % _("clinical trial was published.")
+        else:
+            message = "%s %s" % (rows_updated, _("clinical trials were published."))
+        self.message_user(request, message)
+
+    publish_action.short_description = _("Publish the selected clinical trials")
 
 class DescriptorAdmin(admin.ModelAdmin):
     list_display = ('trial_identifier','vocabulary','code', 'text')
