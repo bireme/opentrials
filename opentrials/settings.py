3459a41299b410fe276633eaccb8bf910bdfd0a9
rafaelbuy <rafaelbuy@gmail.com>
2010-12-02 18:10:55 +0000

    added markdown in displaying the content (faq, news and flatpages: about, terms of use and site-description), close #220

--- opentrials/settings.py
+++ opentrials/settings.py
@@ -105,6 +105,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.flatpages',
+    'django.contrib.markup',
     'vocabulary',
     'repository',
     'reviewapp',
