2df53190b5b9fabda0fe28cc7409228dc21afe08
rafaelbuy <rafaelbuy@gmail.com>
2010-12-13 20:00:36 +0000

    created cache to store the latest tweets (expires in 5 min), close #227

--- context_processors/opentrials.py
+++ context_processors/opentrials.py
@@ -1,4 +1,7 @@
 import settings
+from django.core.cache import cache
+import urllib
+from django.utils.safestring import mark_safe
 
 def polyglot(request):
     context_extra = {}
@@ -24,3 +27,20 @@ def google_analytics(request):
         context_extra['GOOGLE_ANALYTICS_ID'] = settings.GOOGLE_ANALYTICS_ID
 
     return context_extra
+    
+def latest_tweets(request):
+    if not settings.TWITTER:
+        return {"tweets": ""}
+    
+    tweets = cache.get('tweets', '')
+    
+    if tweets:
+        return {"tweets": tweets}
+
+    tweets = urllib.urlopen("http://twitter.com/statuses/user_timeline/"+ settings.TWITTER +".json?callback=twitterCallback2&count=1").readlines()
+    if len(tweets) > 0:
+        tweets = mark_safe(tweets[0])
+        cache.set('tweets', tweets, settings.TWITTER_TIMEOUT)
+
+    return {"tweets": tweets}
+
