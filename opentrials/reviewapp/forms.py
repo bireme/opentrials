00cb94e9b120388178c2398b7c4863a14200ee07
alvesjnr <alvesjunior.antonio@gmail.com>
2011-03-30 17:58:57 +0000

    fixing bug #269

--- opentrials/reviewapp/forms.py
+++ opentrials/reviewapp/forms.py
@@ -9,7 +9,8 @@ from opentrials.reviewapp.models import UserProfile
 from opentrials.reviewapp.models import Attachment, Submission
 from django import forms
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
+
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.forms.formsets import formset_factory, BaseFormSet
